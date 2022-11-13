38aaf94c47fe194df2259ed6cf5f381c182b8d80
MaurilioSemeao <106936169+MaurilioSemeao@users.noreply.github.com>
2022-11-12 23:28:39 -0300

    Adiciona Dark e Light mode

---
+++ pages/video.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { ColorModeContext } from "../src/components/Menu/components/ColorMode"
+
+export default function Video() {
+    const contexto = React.useContext(ColorModeContext)
+    return (
+        <div>
+            {contexto.mode}
+            <button onClick={() => contexto.toggleMode()}>mudar</button>
+        </div>
+    )
+}
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import DarkModeSwitch from "./components/DarkModeSwitch";
 import Search from "./components/Search";
 
 const StyledMenu = styled.header`
@@ -32,6 +33,7 @@ export default function Menu({ valorDaFiltro, setValorDaFiltro }) {
         <Logo />
       </div>
       <Search valorDaFiltro={valorDaFiltro} setValorDaFiltro={setValorDaFiltro} />
+      <DarkModeSwitch />
     </StyledMenu>
   );
 }
